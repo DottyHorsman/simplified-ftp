74458d972ea3a67e37eeb1f2fe597612463add7b
dotty <dottyhorsman@yahoo.com>
2022-02-24 20:43:52 -0500

    fix commit

---
+++ src/Client.java
@@ -0,0 +1,226 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+//import java.util.Random;
+import java.util.Scanner;
+
+public class Client {
+
+    //size of the reply codes from the server
+    //(reply code indicates whether the client request was accepted or rejected by server)
+    private final static int SERVER_CODE_LENGTH = 1;
+
+    public static void main(String[] args) throws IOException{
+
+        if (args.length != 2) {
+            System.err.println("Usage: java Client <server_IP> <server_port>");
+            System.exit(0);
+        }
+
+        int serverPort = Integer.parseUnsignedInt(args[1]);
+        String serverAddr = args[0];
+
+        String command;
+        do{
+            Scanner keyboard = new Scanner(System.in);
+            System.out.println("enter a command (D, G, L, R, or Q):");
+            //Commands are NOT case-sensitive.
+            command = keyboard.next().toUpperCase();
+            keyboard.nextLine();
+
+            String fileName;
+
+            switch (command){
+                case "L":
+                    //List all files (ignoring directories) in the server directory
+                    //(file name : file size)
+                    ByteBuffer buffer = ByteBuffer.wrap("L".getBytes());
+                    SocketChannel channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(serverAddr, serverPort));
+                    //System.out.println("TCP connection established.");
+
+                    //The random sleep is for testing purpose only!
+                    // try {
+                    //    Thread.sleep(new Random().nextInt(20000));
+                    // }catch(InterruptedException e){;}
+
+                    //read from the buffer into the channel
+                    channel.write(buffer);
+
+                    //before writing to buffer, clear buffer
+                    //("position" set to zero, "limit" set to "capacity")
+                    buffer.clear();
+
+                    int bytesRead;
+                    //read will return -1 if the server has closed the TCP connection
+                    // (when server has done sending)
+                    if (serverCode(channel).equals("F")){
+                        System.out.println("Server rejected the request.");
+                    }else {
+                        ByteBuffer data = ByteBuffer.allocate(1024);
+                        while( (bytesRead = channel.read(data)) != -1) {
+                            //before reading from buffer, flip buffer
+                            //("limit" set to current position, "position" set to zero)
+                            data.flip();
+                            byte[] a = new byte[bytesRead];
+                            //copy bytes from buffer to array
+                            //(all bytes between "position" and "limit" are copied)
+                            data.get(a);
+                            String serverMessage = new String(a);
+                            System.out.println(serverMessage);
+                        }
+                    }
+                    channel.close();
+                    break;
+
+
+
+
+
+
+
+
+
+                case "D": //Delete a file
+                    //Ask the user for the file name
+                    System.out.println("enter file name you want to delete:");
+                    fileName = keyboard.nextLine();
+
+                    //Create tcp channel + connect to server
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(serverAddr, serverPort));
+
+                    //make buffer
+                    buffer = ByteBuffer.wrap( ("D"+fileName).getBytes() );
+
+                    //send buffer to server
+                    channel.write(buffer);
+
+                    //shut down sending to server
+                    channel.shutdownOutput();
+
+                    //delete said file in server
+
+                    //Receive server response
+                    //Notify the user whether the operation is successful
+                    if (serverCode(channel).equals("F"))
+                    {
+                        System.out.println("Server rejected the request.");
+                    }
+                    else
+                    {
+                        System.out.println("Server accepted the request.");
+                    }
+
+                    channel.close();
+                    break;
+
+
+
+
+
+
+
+
+                case "G": //Get a file from the server
+                    //Ask the user for the file name
+                    System.out.println("enter file name you want to get:");
+                    fileName = keyboard.nextLine();
+
+                    //Create tcp channel + connect to server
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(serverAddr, serverPort));
+
+                    //make buffer
+                    buffer = ByteBuffer.wrap( ("G"+fileName).getBytes() );
+
+                    //send buffer to server
+                    channel.write(buffer);
+
+                    //shut down sending to server
+                    channel.shutdownOutput();
+
+                    //download
+
+
+
+
+                    //Notify the user whether the operation is successful
+                    break;
+
+
+
+
+
+
+
+
+                case "R": //Rename a file
+                    //Ask the user for the original file name
+                    System.out.println("enter file name you want to rename:");
+                    fileName = keyboard.nextLine();
+                    //and the new file name.
+                    System.out.println("enter new name for file:");
+                    String newfileName = keyboard.nextLine();
+
+                    //Create tcp channel + connect to server
+                    channel = SocketChannel.open();
+                    channel.connect(new InetSocketAddress(serverAddr, serverPort));
+
+                    //make buffer
+                    buffer = ByteBuffer.wrap( ("R"+fileName+"###"+newfileName).getBytes() );
+
+                    //send buffer to server
+                    channel.write(buffer);
+
+                    //shut down sending to server
+                    channel.shutdownOutput();
+
+                    //delete said file in server
+
+                    //Receive server response
+                    //Notify the user whether the operation is successful
+                    if (serverCode(channel).equals("F"))
+                    {
+                        System.out.println("Server rejected the request.");
+                    }
+                    else
+                    {
+                        System.out.println("Server accepted the request.");
+                    }
+
+                    channel.close();
+                    break;
+
+
+
+
+
+                default:
+                    if (!command.equals("Q")){
+                        System.out.println("Unknown command!");
+                    }
+            }
+        }while(!command.equals("Q"));
+    }
+
+    private static String serverCode(SocketChannel channel) throws IOException{
+        ByteBuffer buffer = ByteBuffer.allocate(SERVER_CODE_LENGTH);
+        int bytesToRead = SERVER_CODE_LENGTH;
+
+        //make sure we read the entire server reply
+        while((bytesToRead -= channel.read(buffer)) > 0);
+
+        //before reading from buffer, flip buffer
+        buffer.flip();
+        byte[] a = new byte[SERVER_CODE_LENGTH];
+        //copy bytes from buffer to array
+        buffer.get(a);
+        String serverReplyCode = new String(a);
+
+        //System.out.println(serverReplyCode);
+
+        return serverReplyCode;
+    }
+}
---
+++ src/MultiplexServer.java
@@ -0,0 +1,212 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MultiplexServer {
+
+    private static final int CLIENT_CODE_LENGTH = 1;
+    private static final int MAX_FILE_NAME_LENGTH = 1024;
+
+    public static void main(String[] args) throws IOException{
+        //open a selector
+        Selector monitor = Selector.open();
+
+        ServerSocketChannel welcomeChannel = ServerSocketChannel.open();
+        welcomeChannel.socket().bind(new InetSocketAddress(2000));
+
+        //configure the serverSocketChannel to be non-blocking
+        //(selector only works with non-blocking channels.)
+        welcomeChannel.configureBlocking(false);
+
+        //register the channel and event to be monitored
+        //this causes a "selection key" object to be created for this channel
+        welcomeChannel.register(monitor, SelectionKey.OP_ACCEPT);
+
+        while (true) {
+            // select() is a blocking call (so there is NO busy waiting here)
+            // It returns only after at least one channel is selected,
+            // or the current thread is interrupted
+            int readyChannels = monitor.select();
+
+            //select() returns the number of keys, possibly zero
+            if (readyChannels == 0) {
+                continue;
+            }
+
+            // elements in this set are the keys that are ready
+            // i.e., a registered event has happened in each of those keys
+            Set<SelectionKey> readyKeys = monitor.selectedKeys();
+
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+
+            while (iterator.hasNext()) {
+
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                if (key.isAcceptable()) {
+                    // OS received a new connection request from some new client
+                    ServerSocketChannel wChannel = (ServerSocketChannel) key.channel();
+                    SocketChannel serveChannel = wChannel.accept();
+
+                    //create the dedicated socket channel to serve the new client
+                    serveChannel.configureBlocking(false);
+
+                    //register the dedicated socket channel for reading
+                    serveChannel.register(monitor, SelectionKey.OP_READ);
+                }
+
+                else if (key.isReadable()) {
+                    //OS received one or more packets from one or more clients
+                    SocketChannel serveChannel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = ByteBuffer.allocate(CLIENT_CODE_LENGTH);
+                    int bytesToRead = CLIENT_CODE_LENGTH;
+
+                    //make sure we read the entire server reply
+                    while((bytesToRead -= serveChannel.read(buffer)) > 0);
+
+                    byte[] a = new byte[CLIENT_CODE_LENGTH];
+                    buffer.flip();
+                    buffer.get(a);
+                    String request = new String(a);
+                    System.out.println("Request from a client: " + request);
+
+                    String fileName;
+
+                    switch(request){
+                        case "L":
+                            //send reply code to indicate request was accepted
+                            sendReplyCode(serveChannel, "S");
+
+                            File[] filesList = new File(".").listFiles();
+                            StringBuilder allFiles = new StringBuilder();
+                            if (filesList != null){
+                                for (File f : filesList){
+                                    //ignore directories
+                                    if (!f.isDirectory()) {
+                                        allFiles.append(f.getName());
+                                        allFiles.append(" : ");
+                                        allFiles.append(f.length());
+                                        allFiles.append("\n");
+                                    }
+                                }
+                            }
+
+                            ByteBuffer data = ByteBuffer.wrap(allFiles.toString().getBytes());
+                            serveChannel.write(data);
+                            serveChannel.close();
+                            break;
+
+
+
+
+
+
+                        case "D": //Delete file
+
+                            //make sure we read the entire server reply
+                            buffer = ByteBuffer.allocate(MAX_FILE_NAME_LENGTH);
+                            while((serveChannel.read(buffer)) >= 0); //runs until the read returns a -1 (on client side)
+
+                            buffer.flip();
+                            //buffer.remaining() tells the # of bytes in the buffer
+                            a = new byte[buffer.remaining()];
+                            buffer.get(a);
+                            fileName = new String(a);
+
+                            File d = new File(fileName);
+                            if(d.delete())
+                            {
+                                //send reply code to indicate request was accepted
+                                sendReplyCode(serveChannel, "S");
+                            }
+                            else
+                            {
+                                //or not
+                                sendReplyCode(serveChannel, "F");
+                            }
+
+                            serveChannel.close();
+                            break;
+
+
+
+
+
+                        case "G": //Send file to client
+
+
+
+                            break;
+
+
+
+
+
+
+
+                        case "R": //Rename file
+
+                            //make sure we read the entire server reply
+                            buffer = ByteBuffer.allocate(MAX_FILE_NAME_LENGTH);
+                            while((serveChannel.read(buffer)) >= 0); //runs until the read returns a -1 (on client side)
+
+                            buffer.flip();
+                            //buffer.remaining() tells the # of bytes in the buffer
+                            a = new byte[buffer.remaining()];
+                            buffer.get(a);
+                            fileName = new String(a);
+
+                            String[] splitArray = fileName.split("###", 2);
+                            fileName = splitArray[0];
+                            String newFileName = splitArray[1];
+
+                            System.out.println(fileName + "###" + newFileName);
+
+
+                            File r = new File(fileName);
+                            File e = new File(newFileName);
+                            if(r.renameTo(e))
+                            {
+                                //send reply code to indicate request was accepted
+                                sendReplyCode(serveChannel, "S");
+                            }
+                            else
+                            {
+                                //or not
+                                sendReplyCode(serveChannel, "F");
+                            }
+
+                            serveChannel.close();
+                            break;
+
+
+
+                        default:
+                            System.out.println("Unknown command!");
+                            //send reply code to indicate request was rejected.
+                            sendReplyCode(serveChannel, "F");
+                    }
+                    //note that calling close() will automatically
+                    // deregister the channel with the selector
+                    serveChannel.close();
+                }
+            }
+        }
+    }
+
+    private static void sendReplyCode (SocketChannel channel, String code) throws IOException{
+        ByteBuffer data = ByteBuffer.wrap(code.getBytes());
+        channel.write(data);
+    }
+}
+
+
